a5b770639bcdb987a01771958a1919c072d9e2c6
Zane Badiss <badiss.zane@etu.univ-amu.fr>
2024-04-18 12:17:27 +0200

    Exercice 6(début)

--- src/main/java/fr/amu/iut/exercice6/IHMPendu.java
+++ src/main/java/fr/amu/iut/exercice6/IHMPendu.java
@@ -1,9 +1,30 @@
 package fr.amu.iut.exercice6;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class IHMPendu extends Application {
+
+    private Scene scene;
+    private BorderPane root;
+
+    TextField tf = new TextField();
+
+
+
+    Dico dico = new Dico();
+    String mot = dico.getMot();
+
+    Label lb = new Label(mot);
+
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -11,10 +32,21 @@ public class IHMPendu extends Application {
         primaryStage.setWidth(500);
         primaryStage.setHeight(550);
 
-        // A completer
+        root = new BorderPane();
 
+
+        for(int i=0;i < lb.();)
+
+        root.setCenter(lb);
+        root.setBottom(tf);
+        scene = new Scene(root);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
+
+
+
+
 
     public static void main(String[] args) {
         launch(args);
